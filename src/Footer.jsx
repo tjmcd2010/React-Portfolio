0b291641d9bb3488f220475366854ded8c68a0fa
Travis McDermott <tjmcd2010@gmail.com>
2024-08-26 16:23:43 -0700

    Updates all aspects of the page, just remaining is the README

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,25 @@
+import Github from './assets/github.png'
+import LinkedIn from './assets/linkedin.png'
+import Twitter from './assets/twitter.png'
 
 function Footer (){
     return(
         <footer>
-            <p>&copy; Travis McDermott's Portfolio</p>
+            <div className="footer-container">
+            
+        <div className="social-links">
+          <a href="https://github.com/tjmcd2010" target="_blank" rel="noopener noreferrer">
+            <img src={Github} alt="GitHub icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/your-linkedin-username" target="_blank" rel="noopener noreferrer">
+            <img src={LinkedIn} alt="LinkedIn icon" />
+          </a>
+
+          <a href="https://x.com/tjmcd2010" target="_blank" rel="noopener noreferrer">
+            <img src={Twitter} alt="Twitter icon" />
+          </a>
+        </div>
+      </div>
         </footer>
     )
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,7 @@
 import resumePDF from  '../assets/resume.pdf'
 function Resume() {
     return (
+        <>
       <div className = "resume">
         
         <p>Click the button below to download my resume:</p>
@@ -8,6 +9,26 @@ function Resume() {
           Download Resume
         </a>
       </div>
+      <h3> Here's a list of tech proficiencies I have acquired</h3>
+
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>SQL</li>
+        <li>NoSQL</li>
+        <li>MongoDB</li>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>Insomnia</li>
+        <li>Render</li>
+        <li>Netlify</li>
+      </ul>
+      
+      </>
     );
   }
 
